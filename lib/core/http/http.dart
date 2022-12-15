96745bd1e77c660c74899bc822d2dd998f5b3405
pengxin.wu <pengxin.wu@smawave.com>
2022-12-15 09:50:29 +0800

    feat: set debugPrint & add token into api request

--- lib/core/http/http.dart
+++ lib/core/http/http.dart
@@ -1,6 +1,7 @@
 import 'package:cookie_jar/cookie_jar.dart';
 import 'package:dio/dio.dart';
 import 'package:dio_cookie_manager/dio_cookie_manager.dart';
+import 'package:flutter/cupertino.dart';
 import '../../config/base_config.dart';
 import 'http_interceptor.dart';
 import 'package:dio/adapter.dart';
@@ -33,7 +34,10 @@ class XHttp {
   static Future get(String url, [Map<String, dynamic>? params]) async {
     Response response;
     if (params != null) {
-      response = await dio.get(url, queryParameters: params);
+      response = await dio.get(url, queryParameters: {
+        ...params,
+        '_csrf_token': '0b21f271-494f-4087-9135-79b968e3efc3'
+      });
     } else {
       response = await dio.get(url);
     }
@@ -43,8 +47,11 @@ class XHttp {
   ///post请求
   static Future post(String url,
       {Map<String, dynamic>? params, Map<String, dynamic>? data}) async {
-    Response response =
-        await dio.post(url, queryParameters: params, data: data);
+    Response response = await dio.post(url,
+        queryParameters: params != null
+            ? {...params, '_csrf_token': '0b21f271-494f-4087-9135-79b968e3efc3'}
+            : null,
+        data: data);
     return response.data;
   }
 
@@ -61,7 +68,7 @@ class XHttp {
       response = await dio.download(urlPath, savePath,
           onReceiveProgress: (int count, int total) {
         //进度
-        print("$count $total");
+        debugPrint("$count $total");
       });
     } on DioError catch (e) {
       handleError(e);
@@ -73,22 +80,22 @@ class XHttp {
   static void handleError(DioError e) {
     switch (e.type) {
       case DioErrorType.connectTimeout:
-        print("连接超时");
+        debugPrint("连接超时");
         break;
       case DioErrorType.sendTimeout:
-        print("请求超时");
+        debugPrint("请求超时");
         break;
       case DioErrorType.receiveTimeout:
-        print("响应超时");
+        debugPrint("响应超时");
         break;
       case DioErrorType.response:
-        print("出现异常");
+        debugPrint("出现异常");
         break;
       case DioErrorType.cancel:
-        print("请求取消");
+        debugPrint("请求取消");
         break;
       default:
-        print("未知错误");
+        debugPrint("未知错误");
         break;
     }
   }
--- lib/core/http/http_interceptor.dart
+++ lib/core/http/http_interceptor.dart
@@ -1,31 +1,31 @@
 import 'package:dio/dio.dart';
+import 'package:flutter/cupertino.dart';
 
 /// 拦截器
-class HttpInterceptors extends InterceptorsWrapper{
-
+class HttpInterceptors extends InterceptorsWrapper {
   @override
   void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
-    print("\n================== 请求数据 ==========================");
-    print("url = ${options.uri.toString()}");
-    print("headers = ${options.headers}");
-    print("params = ${options.data}");
+    debugPrint("\n================== 请求数据 ==========================");
+    debugPrint("url = ${options.uri.toString()}");
+    debugPrint("headers = ${options.headers}");
+    debugPrint("params = ${options.data}");
     return super.onRequest(options, handler);
   }
 
   @override
   void onResponse(Response response, ResponseInterceptorHandler handler) {
-    print("\n================== 响应数据 ==========================");
-    print("code = ${response.statusCode}");
-    print("data = ${response.data}");
+    debugPrint("\n================== 响应数据 ==========================");
+    debugPrint("code = ${response.statusCode}");
+    debugPrint("data = ${response.data}");
     return super.onResponse(response, handler);
   }
 
   @override
   void onError(DioError err, ErrorInterceptorHandler handler) {
-    print("\n================== 错误响应数据 ======================");
-    print("type = ${err.type}");
-    print("message = ${err.message}");
-    print("stackTrace = ${err.error}");
+    debugPrint("\n================== 错误响应数据 ======================");
+    debugPrint("type = ${err.type}");
+    debugPrint("message = ${err.message}");
+    debugPrint("stackTrace = ${err.error}");
     return super.onError(err, handler);
   }
 }

98982cbfd1db2af2dff50f1d7d6807d8ca764df9
xiangtong.zhang <xiangtong.zhang@smawave.com>
2023-01-06 17:20:57 +0800

    优化发现设备

--- lib/pages/get_equipment/index.dart
+++ lib/pages/get_equipment/index.dart
@@ -69,18 +69,23 @@ class _MyWidgetState extends State<Equipment> {
       'param':
           '["systemProductModel","systemVersionHw","systemVersionRunning","systemVersionUboot","systemVersionSn","networkLanSettingsMac","networkLanSettingIp","networkLanSettingMask","systemRunningTime"]'
     };
-    setState(() {
-      equipmentData = EquipmentData(
-          systemProductModel: null,
-          systemVersionRunning: '',
-          systemVersionSn: '');
-    });
+    // setState(() {
+    //   equipmentData = EquipmentData(
+    //       systemProductModel: null,
+    //       systemVersionRunning: '',
+    //       systemVersionSn: '');
+    // });
     XHttp.get('/pub/pub_data.html', data).then((res) {
       try {
         var d = json.decode(res.toString());
-        setState(() {
-          equipmentData = EquipmentData.fromJson(d);
-        });
+        print('wwwwww${equipmentData.systemVersionSn}');
+        if (equipmentData.systemVersionSn == null ||
+            equipmentData.systemVersionSn !=
+                EquipmentData.fromJson(d).systemVersionSn) {
+          setState(() {
+            equipmentData = EquipmentData.fromJson(d);
+          });
+        }
       } on FormatException catch (e) {
         debugPrint(e.toString());
       }

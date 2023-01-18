5a5ca59563b937ce4908b5c45056249addd08c5b
jun.kang <jun.kang@smawave.com>
2023-01-18 19:17:22 +0800

    baseUrl地址修改

--- lib/config/base_config.dart
+++ lib/config/base_config.dart
@@ -6,8 +6,7 @@ class BaseConfig {
   /// 服务器路径
   // static const baseUrl = "https://www.smawavelogin.com";
 
-  static const baseUrl = "http://192.168.225.11";
-
+  static const baseUrl = "https://172.16.20.144";
   /// 版本更新路径
   static const updateUrl = "$baseUrl/";
 

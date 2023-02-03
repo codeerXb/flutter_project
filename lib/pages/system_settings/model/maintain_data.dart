13db369933bdae49e9c8c9ca6b8217af23e93f14
lin <1822995446@qq.com>
2023-02-03 11:53:08 +0800

    维护设置页面调整，重启定时接口调试（工作日暂时有问题）

--- lib/pages/system_settings/model/maintain_data.dart
+++ lib/pages/system_settings/model/maintain_data.dart
@@ -13,3 +13,29 @@ class MaintainData {
     return data;
   }
 }
+
+// 重启定时 获取
+class MaintainTrestsetData {
+  String? systemScheduleRebootEnable;
+  String? systemScheduleRebootDays;
+  String? systemScheduleRebootTime;
+
+  MaintainTrestsetData(
+      {this.systemScheduleRebootEnable,
+      this.systemScheduleRebootDays,
+      this.systemScheduleRebootTime});
+
+  MaintainTrestsetData.fromJson(Map<String, dynamic> json) {
+    systemScheduleRebootEnable = json['systemScheduleRebootEnable'];
+    systemScheduleRebootDays = json['systemScheduleRebootDays'];
+    systemScheduleRebootTime = json['systemScheduleRebootTime'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['systemScheduleRebootEnable'] = systemScheduleRebootEnable;
+    data['systemScheduleRebootDays'] = systemScheduleRebootDays;
+    data['systemScheduleRebootTime'] = systemScheduleRebootTime;
+    return data;
+  }
+}

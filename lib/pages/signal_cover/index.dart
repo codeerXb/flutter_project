2c717f0fbd08fc2c3d1ceac1bd4cdc9ce52a606c
pengxin.wu <pengxin.wu@smawave.com>
2023-06-20 16:47:26 +0800

    feat: 限制拖拽的距离

--- lib/pages/signal_cover/index.dart
+++ lib/pages/signal_cover/index.dart
@@ -810,24 +810,32 @@ class _GridWidgetState extends State<GridWidget> {
                     if (rectData.selectedEdge == 'top') {
                       // 拖拽上边缘
                       // change top & height
-                      rectData.offsetY += details.delta.dy;
-                      rectData.height -= details.delta.dy;
+                      if (rectData.height - details.delta.dy > 30) {
+                        rectData.offsetY += details.delta.dy;
+                        rectData.height -= details.delta.dy;
+                      }
                       Vibration.vibrate(duration: 20, amplitude: 225);
                     } else if (rectData.selectedEdge == 'left') {
                       // 拖拽左边缘
                       // change left & width
-                      rectData.offsetX += details.delta.dx;
-                      rectData.width -= details.delta.dx;
+                      if (rectData.width - details.delta.dx > 30) {
+                        rectData.offsetX += details.delta.dx;
+                        rectData.width -= details.delta.dx;
+                      }
                       Vibration.vibrate(duration: 20, amplitude: 225);
                     } else if (rectData.selectedEdge == 'right') {
                       // 拖拽右边缘
                       // change width
-                      rectData.width += details.delta.dx;
+                      if (rectData.width + details.delta.dx > 30) {
+                        rectData.width += details.delta.dx;
+                      }
                       Vibration.vibrate(duration: 20, amplitude: 225);
                     } else if (rectData.selectedEdge == 'bottom') {
                       // 拖拽下边缘
                       // change height
-                      rectData.height += details.delta.dy;
+                      if (rectData.height + details.delta.dy > 30) {
+                        rectData.height += details.delta.dy;
+                      }
                       Vibration.vibrate(duration: 20, amplitude: 225);
                     } else {
                       // 拖拽其他地方

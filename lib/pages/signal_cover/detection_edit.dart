1df6dc52f139e15083274c931640c4b732f5796e
pengxin.wu <pengxin.wu@smawave.com>
2023-06-21 10:36:40 +0800

    feat: wifi覆盖图首页布局

--- lib/pages/signal_cover/detection_edit.dart
+++ lib/pages/signal_cover/detection_edit.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:ui' as ui;
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_template/core/http/http_app.dart';
+import 'package:flutter_template/core/utils/shared_preferences_util.dart';
 import 'package:get/get.dart';
 import '../../core/widget/custom_app_bar.dart';
 import '../../generated/l10n.dart';
+import 'package:card_swiper/card_swiper.dart';
 
 class TestEdit extends StatefulWidget {
   const TestEdit({Key? key}) : super(key: key);
@@ -15,15 +20,51 @@ class _TestEditState extends State<TestEdit> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar:
-            customAppbar(context: context, title: S.current.DetectionAndEdit),
+        appBar: customAppbar(
+          context: context,
+          title: S.current.DetectionAndEdit,
+          backgroundColor: Colors.blue,
+          titleColor: Colors.white,
+        ),
         body: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             Padding(padding: EdgeInsets.only(top: 20.w)),
 
-            Image.asset(
-              'assets/images/signalcover.jpg',
+            SizedBox(
+              width: 1.sw,
+              height: 640.w,
+              child: Swiper(
+                itemBuilder: (BuildContext context, int index) {
+                  // 返回一个widget,传入index
+                  // 根据索引值查询不同楼层数据并绘制
+                  // 这个类需要筛选所有需要的数据，绘制layout
+                  // 绘制路由器
+                  // 根据snr和噪声按照衰减公式绘制图形
+                  // return Image.asset(
+                  //   'assets/images/signalcover.jpg',
+                  // );
+                  return Center(
+                    child: Container(
+                      width: 1.sw - 80.w,
+                      height: 640.w,
+                      // 圆角
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: const Color.fromARGB(255, 250, 250, 250),
+                      ),
+                      child: CoverChart(
+                        floorIndex: index + 1,
+                      ),
+                    ),
+                  );
+                },
+                itemCount: 3,
+                control: const SwiperControl(
+                    size: 20,
+                    color: Colors.black87,
+                    disableColor: Colors.black38),
+              ),
             ),
 
             Padding(padding: EdgeInsets.only(top: 20.w)),
@@ -100,5 +141,166 @@ class _BottomButtonState extends State<BottomButton> {
         ),
       ),
     );
+  }
+}
+
+// 获取对应楼层房屋信号数据
+Future<dynamic> getData(floorIndex) async {
+  try {
+    var sn = await sharedGetData('deviceSn', String);
+    var data = await App.get('/platform/wifiJson/getOne/$sn');
+    List<dynamic> filteredList = List.from(data['wifiJson']['list'])
+        .where((item) => item['floorId'] == floorIndex.toString())
+        .toList();
+    return filteredList;
+  } catch (err) {
+    debugPrint(err.toString());
+  }
+  return null;
+}
+
+// --- 信号强度覆盖图（热力图） ---
+class CoverChart extends StatefulWidget {
+  const CoverChart({Key? key, required this.floorIndex}) : super(key: key);
+  final int floorIndex;
+
+  @override
+  State<StatefulWidget> createState() => _CoverChartState();
+}
+
+class _CoverChartState extends State<CoverChart> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // 绘制layout、router
+    return FutureBuilder(
+      future: getData(widget.floorIndex),
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          if (snapshot.hasError) {
+            return Text('Error: ${snapshot.error}');
+          } else {
+            var data = snapshot.data;
+            // 在这里使用数据进行绘图
+            return Stack(
+              children: [
+                Positioned(
+                  left: (data != null &&
+                          data.isNotEmpty &&
+                          data[0]['routerX'] != null)
+                      ? data[0]['routerX']
+                      : 0.5.sw,
+                  top: (data != null &&
+                          data.isNotEmpty &&
+                          data[0]['routerY'] != null)
+                      ? data[0]['routerY']
+                      : 320.w,
+                  child: Image.asset('assets/images/icon_homepage_route.png'),
+                ),
+                SizedBox(
+                  height: 640.w,
+                  width: 1.sw - 20.w,
+                  child: CustomPaint(
+                    painter: Layout(data ?? []),
+                  ),
+                ),
+              ],
+            );
+          }
+        } else {
+          return const CircularProgressIndicator();
+        }
+      },
+    );
+  }
+}
+
+// --- 房型图 ---
+class Layout extends CustomPainter {
+  Layout(this.floorInfo) : super();
+  final List<dynamic> floorInfo;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..strokeWidth = 2
+      ..color = const Color.fromARGB(255, 49, 49, 49)
+      ..style = PaintingStyle.stroke;
+    List<dynamic> data = floorInfo.isNotEmpty
+        ? floorInfo
+        : [
+            {
+              "offsetX": 0.0,
+              "offsetY": 0.0,
+              "width": 100.0,
+              "height": 100.0,
+              "name": "",
+            }
+          ];
+
+    // 计算放置所有方块之后占用的宽高
+    double left = double.infinity;
+    double right = double.negativeInfinity;
+    double top = double.infinity;
+    double bottom = double.negativeInfinity;
+
+    for (var block in data) {
+      if (block['offsetX'] - 1200 < left) {
+        left = block['offsetX'] - 1200;
+      }
+      if (block['offsetX'] - 1200 + block['width'] > right) {
+        right = block['offsetX'] - 1200 + block['width'];
+      }
+      if (block['offsetY'] - 1200 < top) {
+        top = block['offsetY'] - 1200;
+      }
+      if (block['offsetY'] - 1200 + block['height'] > bottom) {
+        bottom = block['offsetY'] - 1200 + block['height'];
+      }
+    }
+
+    double totalWidth = right - left;
+    double totalHeight = bottom - top;
+    printInfo(info: 'width${size.width}height${size.height}');
+    // 计算缩放比例
+    double scale = totalWidth / 1.sw > totalHeight / 640.w
+        ? 1.sw / totalWidth
+        : 640.w / totalHeight;
+    for (var item in data) {
+      // 绘制矩形
+      final rect = Rect.fromLTWH(
+        (item['offsetX'] - 1200) * scale,
+        (item['offsetY'] - 1200) * scale,
+        item['width'] * scale,
+        item['height'] * scale,
+      );
+
+      canvas.drawRect(rect, paint);
+
+      // 绘制文字
+      final text = TextSpan(
+        text: item['name'].toString(),
+        style: const TextStyle(color: Color.fromARGB(255, 100, 100, 100)),
+      );
+      final textPainter = TextPainter(
+        text: text,
+        textDirection: TextDirection.ltr,
+      );
+      textPainter.layout();
+      textPainter.paint(
+        canvas,
+        Offset((item['offsetX'] - 1200) * scale + item['width'] * scale / 2 - 4,
+            (item['offsetY'] - 1200) * scale + item['height'] * scale / 2 - 8),
+      );
+    }
+  }
+
+  @override
+  bool shouldRepaint(Layout oldDelegate) {
+    return false;
   }
 }
